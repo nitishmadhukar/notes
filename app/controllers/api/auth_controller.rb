2f8968b9e2e1a17d79a92727f3f6e9beec1dd9cb
Nitish Vantha <nitish@Nitishs-MacBook-Pro-2.local>
2023-03-10 08:45:39 -0500

    Add email to response

--- app/controllers/api/auth_controller.rb
+++ app/controllers/api/auth_controller.rb
@@ -9,7 +9,7 @@ module Api
     def signup
       user = User.new(user_params)
       if user.save
-        render json: { message: 'User created successfully' }, status: :created
+        render json: { message: 'User created successfully', email: user_params['email'] }, status: :created
       else
         render json: { error: user.errors.full_messages.join(', ') }, status: :unprocessable_entity
       end
